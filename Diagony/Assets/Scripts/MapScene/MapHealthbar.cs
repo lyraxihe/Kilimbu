ddde7562f15ebfc3b0b6f7ed78ab7f4d64b2b2bd
lyraxihe <129889894+lyraxihe@users.noreply.github.com>
2024-01-22 22:04:40 +0100

    BET

--- Diagony/Assets/Scripts/MapScene/MapHealthbar.cs
+++ Diagony/Assets/Scripts/MapScene/MapHealthbar.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MapHealthbar : MonoBehaviour
 {
