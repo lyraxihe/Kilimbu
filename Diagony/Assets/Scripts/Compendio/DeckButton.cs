20cb4045946c4c37f7d10711b5800fd403e0f560
felipeweil <130854583+felipeweil@users.noreply.github.com>
2024-05-03 00:12:33 +0200

    Sonido Botones part1
    he metido el sonido de botones en las escena de Menu Principal, en el Mapa y en el singleton de Settings.
    Falta por poner en la Tienda, Combate y ChestScene. El barrido sonoro entre escenas hay que arreglarlo porque suena raro.
    Hay que terminar el resto de la programación de audio.

--- Diagony/Assets/Scripts/Compendio/DeckButton.cs
+++ Diagony/Assets/Scripts/Compendio/DeckButton.cs
@@ -9,21 +9,29 @@ public class DeckButton : MonoBehaviour
 
     [SerializeField] GameObject ScrollAreaCompendio;
     [SerializeField] Sprite VolverIcon;
+    [SerializeField] Sprite VolverIconHighlight;
+    [SerializeField] Sprite VolverIconPressed;
     [SerializeField] Sprite CartasIcon;
+    [SerializeField] Sprite CartasIconHighlight;
+    [SerializeField] Sprite CartasIconPressed;
+    [SerializeField] Sprite CartasIconDisabled;
     [SerializeField] GameObject Map;
     [SerializeField] GameObject PauseButton;
+    private SpriteState deckButtonState;
+    private Button button;
     //[SerializeField] TMP_Text ButtonText;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        button = GetComponent<Button>();
+        deckButtonState = button.spriteState;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnClick()
@@ -33,6 +41,10 @@ public class DeckButton : MonoBehaviour
         {
             //ButtonText.text = "Volver";
             gameObject.GetComponent<Image>().sprite = VolverIcon;
+            deckButtonState.pressedSprite = VolverIconPressed;
+            deckButtonState.highlightedSprite = VolverIconHighlight;
+            button.spriteState = deckButtonState;
+
             Map.SetActive(false);
             PauseButton.SetActive(false);
         }
@@ -40,6 +52,10 @@ public class DeckButton : MonoBehaviour
         {
             //ButtonText.text = "Mis Cartas";
             gameObject.GetComponent<Image>().sprite = CartasIcon;
+            deckButtonState.disabledSprite = CartasIconDisabled;
+            deckButtonState.pressedSprite = CartasIconPressed;
+            deckButtonState.highlightedSprite = CartasIconHighlight;
+            button.spriteState = deckButtonState;
             Map.SetActive(true);
             PauseButton.SetActive(true);
         }
